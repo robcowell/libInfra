8ba1dac96ccc6d247adf50ec45374a32fb71b441
Rob Cowell <rob@rob-cowell.com>
2013-10-22 08:55:25 +0100

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libInfra;
+using System.Data;
+
+namespace TestInfra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InfraWrapper wrapper = new InfraWrapper();
+                wrapper.Login(@"EU Business Services", "TEST1234", "VSM");
+
+                //Do things
+                if (wrapper.SessionID != String.Empty)
+                {
+                    DataSet callresult = wrapper.RetrieveCallInfo("1638021");
+                    DataTable dt = callresult.Tables[0];
+                    String calldetails = ReturnDataFromResultSet(dt, "PROBLEM_DESC");
+                    Console.WriteLine(calldetails);
+                    wrapper.Logout(wrapper.SessionID);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
+            Console.ReadLine();
+        }
+
+        private static String ReturnDataFromResultSet(DataTable dt, String fieldname)
+        {
+            if (dt == null)
+            {
+                return String.Empty;
+            }
+
+            // Datatable column 0 is the field name in the cell table
+            // Datatable column 1 is the field value in the cell table
+
+            String fName = String.Empty;
+            String fValue = String.Empty;
+
+            for (int rowcount = 0; rowcount <= dt.Rows.Count; rowcount++)
+            {
+                fName = dt.Rows[rowcount].ItemArray[0].ToString();
+                fValue = dt.Rows[rowcount].ItemArray[1].ToString();
+                if (fName == fieldname)
+                {
+                    return fValue;
+                }
+            }
+
+            return fValue;
+        }
+    }
+}
---
+++ libInfra/InfraWrapper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libInfra
+{
+    public class InfraWrapper
+    {
+        private static ServiceManagerClient client;
+        private String sUsername = String.Empty;
+        private String sPassword = String.Empty;
+        private String sDatabase = String.Empty;
+        private String sSessionID = String.Empty;
+
+        public static ServiceManagerClient Client
+        {
+            get
+            {
+                return client ?? (client = new ServiceManagerClient());
+            }
+        }
+
+        public string SessionID
+        {
+            get
+            {
+                return sSessionID;
+            }
+            set
+            {
+                sSessionID = value;
+            }
+        }
+
+        public InfraWrapper()
+        {
+            
+        }
+
+        public APIReturn Login(String sUsername, String sPassword, String sDatabase)
+        {
+            APIReturn apireturn = APIReturn.API_ERROR_LOGINFAIL;
+            String sMessage = String.Empty;
+            String sID = String.Empty;
+
+            try
+            {
+                apireturn = Client.Login(sUsername,sPassword,sDatabase,out sMessage,out sID);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            sSessionID = sID;
+
+            return apireturn;
+        }
+
+        public APIReturn Logout(String sessionID)
+        {
+            APIReturn apireturn = APIReturn.API_ERROR;
+
+            String sMessage = String.Empty;
+
+            try
+            {
+                apireturn = Client.Logout(sessionID, out sMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return apireturn;
+        }
+
+        public DataSet RetrieveCallInfo(String callnumber)
+        {
+            CallRetrieveRequest callretrieverequest = new CallRetrieveRequest();
+            callretrieverequest.sID = sSessionID;
+            callretrieverequest.lEntityRef = buildLookupString(callnumber);
+
+            CallRetrieveResponse callretrieveresponse = new CallRetrieveResponse();
+
+            String sMessage = String.Empty;
+            DataSet resultset = new DataSet();
+            try
+            {
+                Client.CallRetrieve(sSessionID, sUsername, sPassword, sDatabase, callretrieverequest.lEntityRef, null, out sMessage, out resultset);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            return resultset;
+        }
+
+        private String buildLookupString(Object input)
+        {
+            String strBuilder = String.Empty;
+            if (input != null)
+            {
+                if (input.GetType() == typeof(int))
+                {
+                    strBuilder = "~" + input.ToString();
+                }
+                else
+                {
+                    strBuilder = input.ToString();
+                }
+            }
+
+            return strBuilder;    
+        }
+
+        
+
+    }
+}
